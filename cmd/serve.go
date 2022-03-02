ce33d2268b130f42f1662b6f7f7670463fa90c37
Gwyn <gwyn@playtechnique.io>
2022-03-01 19:04:31 -0700

    Adds ability to add a hostconfig yml file.

--- cmd/serve.go
+++ cmd/serve.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/docker/docker/api/types/container"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"jinx/src/jinkiesengine"
@@ -10,13 +11,14 @@ import (
 
 var (
 	containerConfigPath = ""
+	hostConfigPath      = ""
+	hostConfig          container.HostConfig
 )
 
 func hydrateFromConfig(configPath string) jinkiesengine.ContainerInfo {
 	var config jinkiesengine.ContainerInfo
 
 	if configPath == "" {
-		config.AutoRemove = true
 		config.ImageName = "jamandbees/jinkies"
 		config.ContainerName = "jinkies"
 		config.ContainerPort = "8080/tcp"
@@ -37,12 +39,33 @@ func hydrateFromConfig(configPath string) jinkiesengine.ContainerInfo {
 	return config
 }
 
+func addConfig(configPath string) container.HostConfig {
+	var config container.HostConfig
+
+	viper.AddConfigPath("./")
+	viper.SetConfigType("yml")
+	viper.SetConfigName(configPath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		config = container.HostConfig{
+			AutoRemove: true,
+		}
+	}
+	viper.Unmarshal(&config)
+
+	return config
+}
+
 // serveCmd represents the serve command
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Subcommands to allow you to start or stop an unconfigured jinkies",
 	Long: `Why would you want an unconfigured instance of jinkies? Any time you want a jenkins instance
-quickly for reasons unrelated to a specific job. Maybe you want to prototype some jcasc settings or something.`,
+quickly for reasons unrelated to a specific job. Maybe you want to prototype some jcasc settings or something.
+
+Maybe you want two instances of jinkies running at once? Use the -c flag to supply an environment variables file. To
+write a blank version of this file, see the 'jinx containerconfig' subcommand.
+`,
 }
 
 var startSubCmd = &cobra.Command{
@@ -50,7 +73,7 @@ var startSubCmd = &cobra.Command{
 	Short: "start jinkies!",
 	Long:  `Starts the unconfigured jinkies container`,
 	Run: func(cmd *cobra.Command, args []string) {
-		jinkiesengine.RunRunRun(hydrateFromConfig(containerConfigPath))
+		jinkiesengine.RunRunRun(hydrateFromConfig(containerConfigPath), addConfig(hostConfigPath))
 	},
 }
 
@@ -68,7 +91,22 @@ func init() {
 	serveCmd.AddCommand(startSubCmd)
 	serveCmd.AddCommand(stopSubCmd)
 
-	serveCmd.PersistentFlags().StringVarP(&containerConfigPath, "containerconfig", "c", "", "Path config file describing your container")
+	serveCmd.PersistentFlags().StringVarP(&containerConfigPath, "containerconfig", "c", "", "Path to config file describing your container")
+	serveCmd.PersistentFlags().StringVarP(&hostConfigPath, "hostconfig", "o", "", "Path to config file describing your container host ")
+
+	if hostConfigPath != "" {
+		_, err := os.Open(hostConfigPath)
+		if err != nil {
+			fmt.Printf("Could not open host config file %v \n", err)
+		}
+	}
+
+	if containerConfigPath != "" {
+		_, error := os.Open(containerConfigPath)
+		if error != nil {
+			fmt.Printf("Could not open container config file %v \n", error)
+		}
+	}
 
 	// Here you will define your flags and configuration settings.
 
