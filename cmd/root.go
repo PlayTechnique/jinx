823a55f818982da588ef29a02e2976f8680dc4d9
Gwyn <gwyn@playtechnique.io>
2022-04-03 08:14:25 -0600

    Identifies the global runtime vs subcommand runtime distinction.
    This involved writing a type for the global runtime that can be
    retrieved in main and passed in to subcommands.
    
    There's now login in the root command to appropriately consume
    the global runtime config file, also.

--- cmd/root.go
+++ cmd/root.go
@@ -5,6 +5,9 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"fmt"
+	"jinx/src/utils"
+	jinxtypes "jinx/types"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -30,14 +33,39 @@ func Execute() {
 	}
 }
 
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
+//Sets up the global configuration struct by first setting the runtime to our known sane defaults,
+//then checking to see whether the user supplies a config file option on the command line. If so,
+//overwrite the global configuration struct with the config file contents.
+//Also checks for the default jinx.yml config file. If it exists in the same directory as the calling process,
+//it is used.
+func SetupGlobalConfig() jinxtypes.JinxData {
+	configFilePath := "jinx.yml"
+	configFileDefault := ""
+	configFileOption := configFileDefault
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.tmp.yaml)")
+	JinxRuntime := jinxtypes.JinxData{PullImages: true, ContainerName: "jinkies"}
 
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	serveCmd.PersistentFlags().StringVarP(&configFileOption, "jinxconfig", "j", configFileOption, "Path to config file for jinx global options")
+
+	//Check if the option flag was passed in and read its value if need be to override the default.
+	configOptionPassedIn := configFileOption != configFileDefault
+
+	if configOptionPassedIn {
+		configFilePath = configFileOption
+	}
+
+	if _, err := os.Open(configFilePath); err == nil {
+		utils.HydrateFromConfig(configFilePath, &JinxRuntime)
+	}
+
+	if configOptionPassedIn {
+		//The end user asked us to verify the existence of a file by giving us the explicit expectation
+		//that this filename exists. The file not existing is an error.
+		fmt.Fprintf(os.Stderr, "%v does not exist", configFilePath)
+	} else {
+		//The jinx.yaml file does not exist, but we only checked as a convenience for the user.
+		//We can safely proceed with the default Jinx Runtime.
+	}
+
+	return JinxRuntime
 }
