b4ddea7c7eef9ff7979fb4492be3708a050aa9b8
Gwyn <gwyn@playtechnique.io>
2023-03-21 06:46:55 -0600

    New failing test - validates that all variables in the template files were rendered

--- src/jinkiesengine/create_layout_test.go
+++ src/jinkiesengine/create_layout_test.go
@@ -1,8 +1,12 @@
 package jinkiesengine
 
 import (
+	"bufio"
 	"github.com/stretchr/testify/assert"
+	"io/fs"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 )
 
@@ -19,4 +23,52 @@ func TestCreateLayout(t *testing.T) {
 	assert.FileExists(t, "configFiles/jinx.yml")
 	assert.FileExists(t, "configFiles/containerconfig.yml")
 	assert.FileExists(t, "configFiles/hostconfig.yml")
+}
+
+func TestCreateLayoutHasContentInAllVariables(t *testing.T) {
+	testDir, _ := os.MkdirTemp("", "")
+	os.Chdir(testDir)
+	defer os.RemoveAll(testDir)
+	err := CreateLayout(testDir)
+
+	assert.Nil(t, err)
+
+	filepath.WalkDir(testDir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if d.IsDir() {
+			return nil
+		} else {
+			f, err := os.Open(path)
+
+			if err != nil {
+				return err
+			}
+
+			defer func() error {
+				err = f.Close()
+
+				if err != nil {
+					panic(err)
+				}
+
+				return nil
+			}()
+
+			// Splits on newlines by default.
+			scanner := bufio.NewScanner(f)
+
+			// https://golang.org/pkg/bufio/#Scanner.Scan
+			for scanner.Scan() {
+				line := scanner.Text()
+
+				if strings.Contains(line, "{{") {
+					assert.FailNowf(t, "File "+path+" contains a template in line "+line, "All template variables are populated")
+				}
+			}
+		}
+		return nil
+	})
 }
